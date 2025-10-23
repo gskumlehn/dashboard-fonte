272f5ad1491be12a0c1d25ae39c0ce639917099e
Gabriel Kumlehn <gskumlehn@gmail.com>
2025-10-23 04:53:59 -0300

    Corrige renderizacao dos dados

--- app/static/js/churnAnalysis.js
+++ app/static/js/churnAnalysis.js
@@ -1,13 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     fetch('/comercial/churn-analysis/data')
         .then(response => response.json())
-        .then(data => {
-            if (!Array.isArray(data)) {
-                console.error("Erro ao buscar dados: 'data' não é um array", data);
-                return;
-            }
-            renderTable(data);
-        })
+        .then(data => renderTable(data))
         .catch(error => console.error('Erro ao buscar dados:', error));
 });
 
@@ -20,29 +14,33 @@ function renderTable(data) {
 
         // Cliente
         const clienteTd = document.createElement('td');
-        clienteTd.textContent = row.ClienteNome;
-        clienteTd.title = row.CNPJ_CPF; // Tooltip com CNPJ/CPF
+        clienteTd.textContent = row.cliente;
         tr.appendChild(clienteTd);
 
         // Última Operação
         const ultimaOperacaoTd = document.createElement('td');
-        ultimaOperacaoTd.textContent = new Date(row.UltimaData).toLocaleDateString('pt-BR');
+        ultimaOperacaoTd.textContent = new Date(row.ultima_operacao).toLocaleDateString('pt-BR');
         tr.appendChild(ultimaOperacaoTd);
 
         // Dias Inativo
         const diasInativoTd = document.createElement('td');
-        diasInativoTd.textContent = row.DiasInativo;
+        diasInativoTd.textContent = row.dias_inativo;
         tr.appendChild(diasInativoTd);
 
         // Volume Histórico
         const volumeHistoricoTd = document.createElement('td');
-        volumeHistoricoTd.textContent = `R$ ${row.VolumeHistorico.toLocaleString('pt-BR', { minimumFractionDigits: 2 })}`;
+        const volumeHistorico = parseFloat(row.volume_historico); // Converte para número
+        if (!isNaN(volumeHistorico)) {
+            volumeHistoricoTd.textContent = `R$ ${volumeHistorico.toLocaleString('pt-BR', { minimumFractionDigits: 2 })}`;
+        } else {
+            volumeHistoricoTd.textContent = 'N/A'; // Valor padrão caso seja inválido
+        }
         tr.appendChild(volumeHistoricoTd);
 
         // Risco de Churn
         const riscoTd = document.createElement('td');
-        riscoTd.textContent = row.RiscoChurn;
-        riscoTd.classList.add(getRiskClass(row.RiscoChurn));
+        riscoTd.textContent = row.risco;
+        riscoTd.classList.add(getRiskClass(row.risco));
         tr.appendChild(riscoTd);
 
         tableBody.appendChild(tr);
